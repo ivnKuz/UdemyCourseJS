6e670ba5094620093bace1cdb98c5b7ec0a1d3f3
CyberProger <booleansandwich@gmail.com>
2023-10-06 22:09:46 +0300

    events

--- advanced_DOM_Bankist/starter/script.js
+++ advanced_DOM_Bankist/starter/script.js
@@ -41,7 +41,7 @@ message.innerHTML = 'We use cookies for impoved functionality and analytics. <bu
 header.append(message)
 
 document.querySelector('.btn--close-cookie').
-addEventListener('click', ()=>{
+addEventListener('click', function() {
   message.remove();
   // message.parentElement.removeChild(message);
 })
@@ -55,6 +55,17 @@ const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', function(e){
   const s1coords = section1.getBoundingClientRect();
+  section1.scrollIntoView({behavior: 'smooth'})
+})
+
+
+
+
+
+//learning material
+
+  //________SCROLING________
+  //from btnScrollTo event listener
   // console.log(s1coords);
   // console.log(e.target.getBoundingClientRect());
   // console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset);
@@ -69,5 +80,22 @@ btnScrollTo.addEventListener('click', function(e){
 
   // });
   //same as 2 above lol, can not work in older browsers tho, note.
-  section1.scrollIntoView({behavior: 'smooth'})
-})
+
+  //______EVENTS_______
+  const h1 = document.querySelector('h1')
+  //delete event
+  //can remove it if moved to the separate function
+  const logH1 = function(e){
+    console.log('AddEventListener: Mouse entered');
+    // h1.removeEventListener('mouseenter', logH1)
+  }
+  h1.addEventListener('mouseenter', logH1)
+  // 
+  setTimeout(() => h1.removeEventListener('mouseenter', logH1), 3000);
+//can do the same with attr function
+  // h1.onmouseenter = function(e){
+  //   console.log('AddEventListener: Mouse entered hiya');
+    
+  // }
+
+
